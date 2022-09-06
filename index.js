e308e800989f8b91ffccb4369bdef5adc79be7b7
mnl187 <mnl187@gmail.com>
2022-09-06 06:29:04 +0200

    adding TodoRecord class

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const {pool} = require("./utils/db");
+const {TodoRecord} = require("./records/todo.record");
 
 
 (async () => {
-
+const firstTodoItem = new TodoRecord({
+    id: 'xyz',
+    title: 'Zrobić dzień 5, Tydzien 4'
+});
+await firstTodoItem.insert();
+console.log(firstTodoItem);
 
     await pool.end();
 })();
--- records/todo.record.js
+++ records/todo.record.js
@@ -1,3 +1,25 @@
 class TodoRecord {
+    constructor(obj) {
+        if (obj.title.trim() < 5) {
+            throw new Error('Todo title should be at least 5 characters.');
+        }
+        if (obj.title.length > 150) {
+            throw new Error('Todo title should be at most than 150 characters');
+        }
 
+        this.title = obj.title;
+        this.id = obj.id;
+    }
+
+    async insert() {
+        await pool.execute('INSERT INTO `tools` VALUES(:id, :title)', {
+            id: this.id,
+            title: this.title,
+        });
+    }
 }
+
+module.exports = {
+    TodoRecord,
+}
+
