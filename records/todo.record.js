df8b4c3f8ce713e3d13adfa2cdfc8d394c1f6902
mnl187 <mnl187@gmail.com>
2022-09-08 18:11:23 +0200

    refactoring code

--- records/todo.record.js
+++ records/todo.record.js
@@ -3,20 +3,23 @@ const {v4: uuid} = require('uuid');
 
 class TodoRecord {
     constructor(obj) {
-        if (obj.title.trim().length < 5) {
-            throw new Error('Todo title should be at least 5 characters.');
-        }
-        if (obj.title.length > 150) {
-            throw new Error('Todo title should be at most than 150 characters');
-        }
-
         this.id = obj.id;
         this.title = obj.title;
+
+        this._validate();
     }
 
-    _validatew() {
-
+    _validate() {
+        if (this.title.trim() < 5) {
+            if (this.title.trim().length < 5) {
+                throw new Error('Todo title should be at least 5 characters.');
+            }
+            if (this.title.length > 150) {
+                throw new Error('Todo title should be at most than 150 characters');
+            }
+        }
     }
+
     async insert() {
         this.id = this.id ?? uuid();
 
@@ -26,15 +29,6 @@ class TodoRecord {
         });
         return this.id;
     }
-
-        async update() {
-        await pool.execute('UPADATE `todos` SET `title` = :title WHERE `id` = :id', {
-            title: this.title,
-            id: this.id,
-        });
-        return this.id;
-    }
-
     async delete() {
         if (!this.id) {
             throw new Error('Todo has no ID.');
@@ -44,12 +38,34 @@ class TodoRecord {
             id: this.id,
         });
     }
+
     static async find(id) {
-        const [results] = await pool.execute('SELECT * FROM `todos` WHERE `id` = :id',{
+        const [results] = await pool.execute('SELECT * FROM `todos` WHERE `id` = :id', {
             id: id,
         });
-        return new TodoRecord(results[0]);
+        return results.length === 1 ? TodoRecord(results[0]) : null;
     }
+
+    static async findAll() {
+        // ...
+    }
+
+    async update() {
+        if (!this.id) {
+            throw new Error('Todo has no ID');
+        }
+        this._validate();
+
+        await pool.execute('UPADATE `todos` SET `title` = :title WHERE `id` = :id', {
+            title: this.title,
+            id: this.id,
+        });
+        return this.id;
+    }
+
+
+
+
 }
 
 module.exports = {
