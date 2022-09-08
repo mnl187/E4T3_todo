91a38ab3f0e761da59eba4fbc688804f508b15cf
mnl187 <mnl187@gmail.com>
2022-09-08 18:47:16 +0200

    adding _checkRecord

--- repositories/todo.repository.js
+++ repositories/todo.repository.js
@@ -1,25 +1,35 @@
 const {v4: uuid} = require("uuid");
 const {pool} = require("../utils/db");
+const {TodoRecord} = require("../records/todo.record");
 
 class TodoRepository {
+    static _checkRecord(record) {
+        if (!(record instanceof TodoRecord)) {
+            throw new Error(`record must be an instance of TodoRecord`);
+        }
+    }
+
     static async insert(record) {
+        TodoRepository._checkRecord(record);
 
-
-        this.id = this.id ?? uuid();
+        record.id = record.id ?? uuid();
 
         await pool.execute('INSERT INTO `todos` VALUES(:id, :title)', {
-            id: this.id,
-            title: this.title,
+            id: record.id,
+            title: record.title,
         });
-        return this.id;
+        return record.id;
     }
-    static delete() {
-        if (!this.id) {
+
+    static async delete(record) {
+        TodoRepository._checkRecord(record);
+
+        if (!record.id) {
             throw new Error('Todo has no ID.');
         }
 
         await pool.execute('DELETE FROM `todos` WHERE `id` = :id', {
-            id: this.id,
+            id: record.id,
         });
     }
 
